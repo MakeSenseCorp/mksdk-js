3d29976dd966097cbd8374fd39a195adc6293f93
Yevgeniy Kiveisha <yevgeniy.kiveisha@gmail.com>
2020-09-09 16:55:10 +0300

    widgets

--- widgets/basic_table.js
+++ widgets/basic_table.js
@@ -6,7 +6,7 @@ function MksBasicTable () {
     this.Body           = "";
     this.Content        = `
         <div class="table-responsive">
-            <table class="table table-striped table-hover">
+            <table class="table table-sm table-striped table-hover">
                 <thead>
                     <tr>
                         [HEAD]
@@ -22,25 +22,25 @@ function MksBasicTable () {
 	return this;
 }
 
-MksBasicModal.prototype.SetSchema = function (schema) {
+MksBasicTable.prototype.SetSchema = function (schema) {
     this.Head = "";
     for (idx = 0; idx < schema.length; idx++) {
-        this.Head += "<th>" + schema[idx] + "</th>";
+        this.Head += "<th scope='col'>" + schema[idx] + "</th>";
     }
 }
 
-MksBasicModal.prototype.SetData = function (data) {
+MksBasicTable.prototype.SetData = function (data) {
 	this.Body = "";
     for (idx = 0; idx < data.length; idx++) {
-        this.Head += "<tr>";
+        this.Body += "<tr><th scope='row'>"+(idx+1)+"</th>";
         for (ydx = 0; ydx < data[idx].length; ydx++) {
-            this.Head += "<td>" + data[idx][ydx] + "</td>";
+            this.Body += "<td>" + data[idx][ydx] + "</td>";
         }
-        this.Head += "</tr>";
+        this.Body += "</tr>";
     }
 }
 
-MksBasicModal.prototype.Build = function (obj) {
+MksBasicTable.prototype.Build = function (obj) {
     this.WorkingObject = obj;
     var html = this.Content;
     html = html.split("[HEAD]").join(this.Head);
@@ -48,7 +48,7 @@ MksBasicModal.prototype.Build = function (obj) {
     obj.innerHTML = html;
 }
 
-MksBasicModal.prototype.Remove = function () {
+MksBasicTable.prototype.Remove = function () {
     if (this.WorkingObject !== undefined && this.WorkingObject !== null) {
 		this.WorkingObject.parentNode.removeChild(this.WorkingObject);
 	}
